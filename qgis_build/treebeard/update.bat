9055b046d44e4f66a6e8695d335c3f91e4e6a467
C. Griego <nobodyliveshere@protonmail.com>
2024-06-26 20:58:59 -0600

    created updater script

--- qgis_build/treebeard/update.bat
+++ qgis_build/treebeard/update.bat
@@ -2,13 +2,21 @@
 setlocal
 
 REM Get the directory where the batch file is located (the dev folder)
-set DEV_DIR=%~dp0
-set PLUGIN_DIR=%USERPROFILE%\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\treebeard
+set "DEV_DIR=%~dp0"
+set "PLUGIN_DIR=%USERPROFILE%\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\treebeard"
 
-echo Syncing updated files from development folder to QGIS plugin directory...
+REM Specify the file to sync
+set "FILENAME=treebeard.py"
 
-REM Use robocopy to copy only new and changed files
-robocopy "%DEV_DIR%" "%PLUGIN_DIR%" /MIR
+REM Debugging output to check paths and file
+echo Development Directory: %DEV_DIR%
+echo Plugin Directory: %PLUGIN_DIR%
+echo File to sync: %FILENAME%
+
+echo Syncing the specified file from development folder to QGIS plugin directory...
+
+REM Use robocopy to copy only the specified file if it has changed
+robocopy "%DEV_DIR%" "%PLUGIN_DIR%" "%FILENAME%" /XO /XC /R:3 /W:10
 
 echo Restart QGIS to see the changes.
 pause
